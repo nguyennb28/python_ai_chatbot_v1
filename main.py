59b82ab224d8b32785cb31241ad567cfdc4e0a94
nguyennb28 <nguyennb.coding@gmail.com>
2025-02-14 11:12:00 +0700

    make first chatbot application

--- main.py
+++ main.py
@@ -2,13 +2,34 @@ from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
 template = """
+Answer the question below.
 
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
 """
 
-# https://www.youtube.com/watch?v=d0o89z134CQ
+# Video youtube: https://www.youtube.com/watch?v=d0o89z134CQ
 
 model = OllamaLLM(model="gemma2:2b")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
 
-result = model.invoke(input="Xin chào giáo viên tiếng anh và ngoại ngữ cấp 3")
-print(result)
+def handle_conversation():
+    context = "You are a super good English teacher in Vietnam."
+    print("Welcome to the AI chatbot! Type 'exit' to quit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+        
+        
+        result = chain.invoke({"context": context, "question": user_input})
+        print("Bot: ", result)
+        context += f"\nUser: {user_input}\nAI: {result}"
 
+# print(handle_conversation())
+if __name__ == "__main__":
+    handle_conversation()
